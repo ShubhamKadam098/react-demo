18c064e9fdf9572f8ab48a6a457a9bf867c2594d
Shubham Kadam <kadamshubham098@gmail.com>
2025-07-18 02:24:32 +0530

    Enhance App component to display environment variables and improve code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const envs = import.meta.env;
 
   return (
     <>
       <div>
+        <h2>Environment Variables:</h2>
+        <pre
+          style={{
+            textAlign: "left",
+            padding: "10px",
+            borderRadius: "4px",
+            fontSize: "12px",
+          }}
+        >
+          {JSON.stringify(envs, null, 2)}
+        </pre>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +41,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
